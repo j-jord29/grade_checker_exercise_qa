d33943899462024a16d6847bdf3ff65962de1003
j-jord29 <j.jordan2649@gmail.com>
2022-06-30 10:28:59 +0100

    Completed Task with Explanations

--- grade_checker_exercise_qa.py
+++ grade_checker_exercise_qa.py
@@ -1,11 +1,16 @@
 def grades(hw_score, ass_score, fin_score):
+    # Sets the Maximum Score a Student can Achieve
     max_hw_score = 25
     max_ass_score = 50
     max_fin_score = 100
-    
+
+    # Sum of the Total of Max Scores    
     max_total = max_hw_score + max_ass_score + max_fin_score
+
+    # Sum of the Total of User Scores
     user_total = hw_score + ass_score + fin_score
 
+    # Checks if the User Scores are Valid
     if (hw_score > max_hw_score) or (ass_score > max_ass_score) or (fin_score > max_fin_score):
         return "Not Allowed"
     else:
