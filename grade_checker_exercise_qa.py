541eeedf39a998583614cb14b8b71d6c9b2463f7
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-07-01 16:49:38 +0100

    fix

--- grade_checker_exercise_qa.py
+++ grade_checker_exercise_qa.py
@@ -1,19 +1,3 @@
-def farts(hw_score, ass_score, fin_score):
-    # Sets the Maximum Score a Student can Achieve
-    max_hw_score = 25
-    max_ass_score = 50
-    max_fin_score = 100
+import webbrowser
 
-    # Sum of the Total of Max Scores    
-    max_total = max_hw_score + max_ass_score + max_fin_score
-
-    # Sum of the Total of User Scores
-    user_total = hw_score + ass_score + fin_score
-
-    # Checks if the User Scores are Valid
-    if (hw_score > max_hw_score) or (ass_score > max_ass_score) or (fin_score > max_fin_score):
-        return "Not Allowed"
-    else:
-        return round(100 * user_total / max_total, 1)
-    
-print(farts(25, 50, 100))
+webbrowser.open('https://www.youtube.com/watch?v=nU5MYvtuIb4')
